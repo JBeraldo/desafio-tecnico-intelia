223daececb0242ad7b806df6d6db3aa9d0fcfc56
JBeraldo <joaoangelo409@gmail.com>
2025-11-07 22:17:34 -0300

    FEAT: Display de erros no form

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,13 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
-    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}}
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+    { provide: STEPPER_GLOBAL_OPTIONS,useValue: { showError: true }}
   ]
 };
